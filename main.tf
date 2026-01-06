d2018806e7037d49010679bde764ff8c38d64d83
arou <bananaumai96@gmail.com>
2026-01-06 05:31:16 +0000

    add tags and test destroy

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
 terraform {
   backend "s3" {
-    bucket = "anzai-terraform-practice-001"
+    bucket = "anzai-terraform-backend"   # backend 用バケット
     key    = "dev/terraform.tfstate"
     region = "ap-northeast-1"
   }
-}
-
-terraform {
-  required_version = ">= 0.14"
 }
 
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "anzai-terraform-practice-001"
+resource "aws_s3_bucket" "practice" {
+  bucket = "anzai-terraform-practice-001"  # 学習用
+  force_destroy = true                       # 中身があっても destroy 可能
 
   tags = {
     Name = "terraform-practice"
     Env  = "dev"
+    Owner = "arou"
   }
 }
